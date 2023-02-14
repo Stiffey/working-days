8ddfb8b6fba2e9f2f0ec8a941df581fc1aa5699a
stiffey <steven.diffey@gmail.com>
2023-02-14 22:16:00 +0000

    Month select update

--- app/app.py
+++ app/app.py
@@ -1,12 +1,15 @@
 from datetime import date, timedelta, datetime
-# import inspect
 import numpy as np
 import json
 import requests
 from calendar import month, monthrange
 from flask import Flask, render_template, request, url_for, redirect
 import os
-# import datetime
+import logging
+logging.basicConfig(level=logging.INFO)
+
+
+# from werkzeug.exceptions import HTTPException
 
 app = Flask(__name__)
 
@@ -23,31 +26,30 @@ def workingDays():
 
     current_month_yyyy_mm = str(datetime.now())[0:7]
     
+    
     try:
         month_year
     except:
         day = "Select a date"
+    
+    # else:
+    if month_year is None:
+        day = "Select a date"
+        logging.info('No month selected')
+        return render_template('index.html', day=day, current_month_yyyy_mm=current_month_yyyy_mm, min_date_yyyy_mm=min_date_yyyy_mm, max_date_yyyy_mm=max_date_yyyy_mm)
     else:
-        if month_year is None:
-            day = "Select a date"
-            return render_template('index.html', day=day, current_month_yyyy_mm=current_month_yyyy_mm, min_date_yyyy_mm=min_date_yyyy_mm, max_date_yyyy_mm=max_date_yyyy_mm)
-        else:
-            month_year_split = month_year.split("-")
-            month = month_year_split[1]
-            year = month_year_split[0]
+        month_year_split = month_year.split("-")
+        month = month_year_split[1]
+        year = month_year_split[0]
 
+        try:
             month_name = months[(int(month  ) - 1)]
-
             monthInput = month
             yearInput = year
-            
             ukHolidayList = []
-
             holidaysJSON = requests.get("https://www.gov.uk/bank-holidays.json")
             ukHolidaysJSON = json.loads(holidaysJSON.text)['england-and-wales']['events']
-
             min_date = ukHolidaysJSON[0]['date']
-            print(min_date)
 
             # for events in ukHolidays:
             eventIterator = 0
@@ -66,35 +68,42 @@ def workingDays():
             edate = date(int(yearInput), int(monthInput), int(daysInMonth))   # end date
 
             # Calculate delta
-            delta = edate - sdate       # as timedelta
+            delta = edate - sdate     # as timedelta
 
-            # Find all of the business days in the month
-            numberOfWorkingDays = 0
-            workingDay = []
-            for i in range(delta.days + 1):  # Look through all days in the month
-                day = sdate + timedelta(days=i)
-                if np.is_busday([day]) and str(day) not in ukHolidayList: # Determine if it's a business day
-                    # print("- " + str(day))
-                    workingDay.append(str(day))
-                    numberOfWorkingDays += 1
+        except:
+            return "Dodgy month"
 
-            # Count all of the UK hoidays
-            public_holidays = []
-            numberOfHolidays = 0
-            for i in range(delta.days + 1):  # Look through all days in the month
-                day = sdate + timedelta(days=i)
-                if str(day) in ukHolidayList: # Determine if it's a uk holiday
-                    public_holidays.append(str(day))
-                    numberOfHolidays += 1
+        # Find all of the business days in the month
+        numberOfWorkingDays = 0
+        workingDay = []
+        for i in range(delta.days + 1):  # Look through all days in the month
+            day = sdate + timedelta(days=i)
+            if np.is_busday([day]) and str(day) not in ukHolidayList: # Determine if it's a business day
+                # print("- " + str(day))
+                workingDay.append(str(day))
+                numberOfWorkingDays += 1
 
-            # Make a list of public holidays
-            public_holiday_list = [ title["title"] for title in ukHolidaysJSON if datetime.strptime(title["date"], '%Y-%m-%d').date() > sdate and datetime.strptime(title["date"], "%Y-%m-%d").date() < edate ]
-        
-            public_holiday_list.reverse()
+        # Count all of the UK hoidays
+        public_holidays = []
+        numberOfHolidays = 0
+        for i in range(delta.days + 1):  # Look through all days in the month
+            day = sdate + timedelta(days=i)
+            if str(day) in ukHolidayList: # Determine if it's a uk holiday
+                public_holidays.append(str(day))
+                numberOfHolidays += 1
 
-            return render_template('index.html', day=workingDay, month_year=month_year, month=month, year=year, numberOfWorkingDays=numberOfWorkingDays, numberOfHolidays=numberOfHolidays, month_name=month_name, public_holidays=public_holidays, public_holiday_list=public_holiday_list, current_month_yyyy_mm=current_month_yyyy_mm, min_date_yyyy_mm=min_date_yyyy_mm, max_date_yyyy_mm=max_date_yyyy_mm)
+        # Make a list of public holidays
+        public_holiday_list = [ title["title"] for title in ukHolidaysJSON if datetime.strptime(title["date"], '%Y-%m-%d').date() > sdate and datetime.strptime(title["date"], "%Y-%m-%d").date() < edate ]
+    
+        public_holiday_list.reverse()
+
+        return render_template('index.html', day=workingDay, month_year=month_year, month=month, year=year, numberOfWorkingDays=numberOfWorkingDays, numberOfHolidays=numberOfHolidays, month_name=month_name, public_holidays=public_holidays, public_holiday_list=public_holiday_list, current_month_yyyy_mm=current_month_yyyy_mm, min_date_yyyy_mm=min_date_yyyy_mm, max_date_yyyy_mm=max_date_yyyy_mm)
 
 # if __name__ == "__main__":
 #     # app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
 #     # app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
 #     app.run('0.0.0.0', port=8080)
+
+@app.errorhandler(404)
+def handle_bad_request(e):
+    return '<h1>Bad URL</h1>', 400
